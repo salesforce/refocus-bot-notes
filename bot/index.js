0933c60c93640c79440c18e714d6ec75d7ac0a21
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-04-19 13:45:25 -0400

    fix contexts

--- bot/index.js
+++ bot/index.js
@@ -5,18 +5,18 @@ var WarRoom = 'http://localhost:3000';
 var socket = io.connect('http://localhost:3000', {reconnect: true});
 
 var bot_identifier = 'WarRoom_Bot';
-var actions;
-var context;
+var BOT_ACTIONS = require('./actions.json');
+var BOT_CONTEXTS = require('./context.json');
 
-socket.emit('Events', bot_identifier, WarRoom, null, bot_identifier + 'is connected', actions, context);
+socket.emit('Events', bot_identifier, WarRoom, null, bot_identifier + 'is connected', BOT_ACTIONS, BOT_CONTEXTS);
 
 socket.on('Events', function (from, to, session, message, actionsJSON, contextJSON) {
 	if(to === bot_identifier){
 		if(message === 'Request new session'){
 			var room = session;
-            socket.emit('Events', bot_identifier, WarRoom, session, bot_identifier + 'performed' + action.name, null, null);
+            socket.emit('Events', bot_identifier, WarRoom, session, bot_identifier + 'performed', null, null);
         }else if(message === 'Request Action'){
-            for(var action : actionsJSON){
+            for(let action of actionsJSON.actions){
                 //do actions
                 if(action.name === 'Function1'){
 
@@ -24,14 +24,14 @@ socket.on('Events', function (from, to, session, message, actionsJSON, contextJS
                 socket.emit('Events', bot_identifier, WarRoom, room, bot_identifier + 'performed' + action.name, null, null);
             }
         }else if(message === 'Request Variable'){
-            for(contextJSON){
+            for(let context of contextJSON.context){
                 //get context
-                socket.emit('Events', bot_identifier, WarRoom, room, bot_identifier + 'is connected', actions, context);
+                var getContext = null;//getContext() function developed here
+                socket.emit('Events', bot_identifier, WarRoom, room, bot_identifier + 'is connected', null, getContext);
             }
         }else if(message === 'Request Variable Update'){
-            for(contextJSON){
+            for(let context of contextJSON.context){
                 //update context
-                socket.emit('Events', bot_identifier, WarRoom, room, bot_identifier + 'is connected', actions, context);
             }
         }
 	}
